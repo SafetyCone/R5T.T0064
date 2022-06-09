5238dc0214de688b76f9981d99dc014014059a8e
davidcoats <david@getsafetycone.com>
2022-06-09 12:39:15 -0700

    Interim changes.

---
+++ source/R5T.T0064/Code/Attributes/DraftServiceDefinitionMarkerAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace R5T.T0064
+{
+    /// <summary>
+    /// Marks an interface as being a draft service definition.
+    /// Drafting allows service definitions to be quickly created when needed, then reviewed and moved to a permanent location at a later time.
+    /// This attribute is useful in surveying draft service definitions to create a kind of TODO list of service definitions that should be reviewed.
+    /// Also allows specifying that an interface is *not* a service definition. (Useful for decorating extraneous intefaces in files that happen to be in the canonical draft service definition files location.)
+    /// Not inherited.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    public sealed class DraftServiceDefinitionMarkerAttribute : Attribute
+    {
+        private readonly bool zIsServiceDefinition;
+        public bool IsServiceDefinition
+        {
+            get
+            {
+                return this.zIsServiceDefinition;
+            }
+        }
+
+
+        public DraftServiceDefinitionMarkerAttribute(
+            bool isServiceDefinition = true)
+        {
+            this.zIsServiceDefinition = isServiceDefinition;
+        }
+    }
+}
---
+++ source/R5T.T0064/Code/Attributes/DraftServiceImplementationMarkerAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace R5T.T0064
+{
+    /// <summary>
+    /// Marks a class as being a draft service implementation.
+    /// Drafting allows service implementations to be quickly created when needed, then reviewed and moved to a permanent location at a later time.
+    /// This attribute is useful in surveying draft service implementations to create a kind of TODO list of service implementations that should be reviewed.
+    /// Also allows marking a class as *not* being a service implementation. (Useful for decorating extraneous classes in files that happen to be in the canonical service implementation code files location.)
+    /// Not inherited.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class DraftServiceImplementationMarkerAttribute : Attribute
+    {
+        private readonly bool zIsServiceImplementation;
+        public bool IsServiceImplementation
+        {
+            get
+            {
+                return this.zIsServiceImplementation;
+            }
+        }
+
+
+        public DraftServiceImplementationMarkerAttribute(
+            bool isServiceImplementation = true)
+        {
+            this.zIsServiceImplementation = isServiceImplementation;
+        }
+    }
+}
